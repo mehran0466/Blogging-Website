2eace7436c65c10a3ce04c84d7283369401cffa5
0466 <l1f21bsse0466@ucp.edu.pk>
2025-01-27 15:03:02 +0500

    initial commit

---
+++ configuration.sh
@@ -0,0 +1,7 @@
+git config --local user.name "0466"
+git config --local user.email "l1f21bsse0466@ucp.edu.pk"
+git branch -m develop
+mkdir src styles
+touch .gitignore
+touch ./src/home.html ./styles/home.css
+npm init -y
